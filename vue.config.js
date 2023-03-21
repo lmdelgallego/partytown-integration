4df392c88c42e27b7e3a79a091846da51439d926
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-03-21 11:55:07 -0500

    👷🏽‍♂️ Add partytown script with webpack

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
+const path = require('path');
 const { defineConfig } = require('@vue/cli-service');
+const CopyPlugin = require('copy-webpack-plugin');
+const partytown = require('@builder.io/partytown/utils');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
-    plugins: [],
+    plugins: [
+      new CopyPlugin({
+        patterns: [{
+          from: partytown.libDirPath(),
+          to: path.join(__dirname, 'public', '~partytown'),
+        }],
+      }),
+    ],
   },
 
 });
